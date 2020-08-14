bf63a85860da526c3bdb3a27635ed1c83a289c06
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-14 01:08:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,5 @@
 # Code your solution here!
-
+require pry
 def prompt_user_for_guess
   puts "I'm thinking of a number from 1-6 including 1 and 6. Can you guess it?"
 end
@@ -10,7 +10,7 @@ def run_guessing_game
   cpu_number = rand(1..6)
   #prompt_user_for_guess
   user_guess = gets.chomp
-  
+  binding.pry
   if cpu_number == user_guess
     puts "You guessed the correct number!"
   elsif user_guess == "exit"
