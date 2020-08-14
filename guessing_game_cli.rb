8eb105371e610bd6a22560c591280fb3e6d4e95b
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-14 01:01:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,22 @@
 # Code your solution here!
+
+def run_guessing_game
+  
+  cpu_number = rand(1..6)
+  prompt_user_for_guess
+  user_guess = get.chomp
+  
+  if cpu_number == user_guess
+    
+  elsif user_guess == "exit"
+    p "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{cpu_number}."
+  end
+    
+end
+
+
+def prompt_user_for_guess
+  puts "I'm thinking of a number from 1-6 including 1 and 6. Can you guess it?"
+end
