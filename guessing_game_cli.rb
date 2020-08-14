0ffb751632afab2f548234487b84d54f99d4eb2b
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-14 01:20:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   cpu_number = rand(1..6)
   #prompt_user_for_guess
   user_guess = gets.chomp
-  binding.pry
+  
   if user_guess.to_i == cpu_number
     puts "You guessed the correct number!"
   elsif user_guess == "exit"
@@ -17,5 +17,5 @@ def run_guessing_game
   end
     
 end
-binding.pry
+
 
